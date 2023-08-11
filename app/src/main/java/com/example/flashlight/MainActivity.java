a879e94a7f88a78eca15ed19c45790b516164a1d
dikshu126 <88445660+dikshu126@users.noreply.github.com>
2023-08-11 15:27:57 +0530

    Final Commit

---
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.flashlight;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity {
+
+    private CameraManager cameraManager;
+    private String cameraId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+
+        Switch switchButton = findViewById(R.id.switchButton);
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                toggleFlashlight(isChecked);
+            }
+        });
+    }
+
+    private void toggleFlashlight(boolean isEnabled) {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                cameraManager.setTorchMode(cameraId, isEnabled);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
